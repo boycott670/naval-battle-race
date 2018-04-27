a2833cd55c0b176f758184dd0cebea18ec8626eb
takingsss12 <takingsss12@gmail.com>
2018-04-27 12:21:00 +0100

    refatored Ship.java

--- src/main/java/com/sqli/nespresso/navalbattle/ships/Ship.java
+++ src/main/java/com/sqli/nespresso/navalbattle/ships/Ship.java
@@ -11,15 +11,15 @@ public class Ship
   public Ship(int displacement, int mast, int canon)
   {
     this.displacement = displacement;
-    this.mast = mast * 1000;
-    this.canon = canon * 100;
+    this.mast = mast;
+    this.canon = canon;
 
-    hp = this.displacement + this.mast + this.canon;
+    hp = this.displacement + this.mast * 1000 + this.canon * 100;
   }
 
   public final double damage()
   {
-    return Math.max(0, 2 * canon);
+    return Math.max(0, 200 * canon);
   }
 
   public final void takeDamage(final double damage)
@@ -31,11 +31,11 @@ public class Ship
   {
     if (mast > 0)
     {
-      mast -= damage;
+      mast -= damage / 1000;
     }
     else if (canon > 0)
     {
-      canon -= damage;
+      canon -= damage / 100;
     }
     else
     {
